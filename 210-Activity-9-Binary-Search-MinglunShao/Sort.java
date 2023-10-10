ac206cd261012a635f30014efc1a06bbe4ddef9c
Minglun Shao <dev.minglunshao@gmail.com>
2023-10-10 09:56:38 -0400

    Add code from Tue, 10 Oct 2023 09:56:38 -0400

---
+++ 210-Activity-9-Binary-Search-MinglunShao/Sort.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+class Sort {
+
+    public static void bubbleSortStrings(ArrayList<String> list) {
+        boolean swap = true;
+        while (swap) {
+            swap = false;
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i).compareToIgnoreCase(list.get(i + 1)) > 0) {
+                    swap = true;
+                    String temp = list.get(i);
+                    list.set(i, list.get(i + 1));
+                    list.set(i + 1, temp);
+
+                }
+            } // for
+        } // while
+    }// end bubbleSortStrings
+
+    public static void bubbleSortDoubles(ArrayList<Double> list) {
+        boolean swap = true;
+        while (swap) {
+            swap = false;
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i) > list.get(i + 1)) {
+                    swap = true;
+                    Double temp = list.get(i);
+                    list.set(i, list.get(i + 1));
+                    list.set(i + 1, temp);
+                }
+            } // for
+        } // while
+    }// end bubbleSortDoubles
+
+    public static void bubbleSortIntegers(ArrayList<Integer> list) {
+        boolean swap = true;
+        while (swap) {
+            swap = false;
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i) > list.get(i + 1)) {
+                    swap = true;
+                    Integer temp = list.get(i);
+                    list.set(i, list.get(i + 1));
+                    list.set(i + 1, temp);
+                }
+            } // for
+        } // while
+    }// end bubbleSortIntegers
+
+    public static void bubbleSortSSI(ArrayList<String> list, ArrayList<String> list2, ArrayList<Integer> list3) {
+        boolean swap = true;
+        while (swap) {
+            swap = false;
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i).compareToIgnoreCase(list.get(i + 1)) > 0) {
+                    swap = true;
+                    String temp = list.get(i);
+                    list.set(i, list.get(i + 1));
+                    list.set(i + 1, temp);
+
+                }
+            } // for
+        } // while
+    }// end bubbleSortStrings
+}// end Sort Class
